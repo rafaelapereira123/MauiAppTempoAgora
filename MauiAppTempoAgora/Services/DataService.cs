5df23e363557fa244fb92bd1ab6198387523ba5d
Rafaela Pereira de Andrade <rp431040@gmail.com>
2026-04-07 00:02:14 -0300

    enviar por push

---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,52 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "7ba260bbffd9bb0cad06ca02f2842409";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddDays((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddDays((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+                    }; //Fecha obj do tempo
+                } // Fecha if se o status do servidor for de sucesso
+
+
+               
+            } // fecha laço using
+            return t;
+        }
+    }
+}
